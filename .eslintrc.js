4fbe05147214f3b8984be82c1287e276b895d54a
Gene Sy <gnsyftw@gmail.com>
2019-05-12 21:51:40 +0800

    Adds auto lint on commit and push, also fixes lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   "extends": [
     "airbnb-base",
+    "eslint:recommended",
     "plugin:react/recommended"
   ],
   "rules": {
     "react/jsx-uses-react": 1
-  }
+  },
+  "env": {
+    "browser": true,
+    "mocha": true
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "plugins": [
+    "react"
+  ]
 };
--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
@@ -13,5 +13,5 @@ export default function Nav() {
         </li>
       </ul>
     </nav>
-  )
+  );
 }
