a0ff036146815c9a5c1697cb7b54e7f20759d563
Matt Vance <mvance43776@gmail.com>
2019-05-20 19:57:34 -0700

    Change icons to functional components

--- src/icons/LiveIcon.js
+++ src/icons/LiveIcon.js
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class LiveIcon extends Component {
-  render() {
-    return (
-      <svg
-        className="live-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 17"
-        width="20"
-        height="17"
-      >
-        <path
-          transform="translate(-2 -3)"
-          d="M 10 20 V 14 h 4 v 6 h 5 V 12 h 3 L 12 3 L 2 12 H 5 v 8 Z"
-        />
-      </svg>
-    );
-  }
+function LiveIcon() {
+  return (
+    <svg
+      className="live-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 17"
+      width="20"
+      height="17"
+    >
+      <path
+        transform="translate(-2 -3)"
+        d="M 10 20 V 14 h 4 v 6 h 5 V 12 h 3 L 12 3 L 2 12 H 5 v 8 Z"
+      />
+    </svg>
+  );
 }
 
 export default LiveIcon;
