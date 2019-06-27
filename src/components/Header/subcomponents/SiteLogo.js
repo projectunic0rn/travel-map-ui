0dbe1163ae433f8bf5781c1f14c9c9ee4e378573
Matt Vance <mvance43776@gmail.com>
2019-06-26 23:53:32 -0700

    Added first logo, changed fonts, fixed dropdown position on large screen

--- src/components/Header/subcomponents/SiteLogo.js
+++ src/components/Header/subcomponents/SiteLogo.js
@@ -1,5 +1,6 @@
 import React from "react";
+import SiteIcon from '../../../icons/SiteIcon';
 
 export default function SiteLogo() {
-  return <div className="site-logo" />;
+  return <div className="site-logo"><SiteIcon /></div>;
 }
