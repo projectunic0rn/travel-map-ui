d9f7a481a107060b053c6758d6ad483649b515dc
Enes <enestasbasi@outlook.com>
2019-08-14 01:06:07 -0400

    Add map scorecard drag functionality

--- src/pages/Home/subcomponents/MapScorecard.js
+++ src/pages/Home/subcomponents/MapScorecard.js
@@ -1,32 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-export default function MapScorecard(props) {
-  const [isPastActive, handlePastActive] = useState(props.activeTimings[0]);
-  const [isFutureActive, handleFutureActive] = useState(props.activeTimings[1]);
-  const [isLiveActive, handleLiveActive] = useState(props.activeTimings[2]);
-  const { tripTimingCounts } = props;
+import DragIcon from "../../../icons/DragIcon";
+
+export default function MapScorecard({
+  tripTimingCounts,
+  sendActiveTimings,
+  activeTimings
+}) {
+  const [isPastActive, handlePastActive] = useState(activeTimings[0]);
+  const [isFutureActive, handleFutureActive] = useState(activeTimings[1]);
+  const [isLiveActive, handleLiveActive] = useState(activeTimings[2]);
 
   function handleTimingClicked(timingCategory) {
     switch (timingCategory) {
       case 0:
-        props.sendActiveTimings([!isPastActive, isFutureActive, isLiveActive]);
+        sendActiveTimings([!isPastActive, isFutureActive, isLiveActive]);
         handlePastActive(!isPastActive);
         break;
       case 1:
-        props.sendActiveTimings([isPastActive, !isFutureActive, isLiveActive]);
+        sendActiveTimings([isPastActive, !isFutureActive, isLiveActive]);
         handleFutureActive(!isFutureActive);
         break;
       case 2:
-        props.sendActiveTimings([isPastActive, isFutureActive, !isLiveActive]);
+        sendActiveTimings([isPastActive, isFutureActive, !isLiveActive]);
         handleLiveActive(!isLiveActive);
         break;
       default:
         break;
     }
   }
+
+  useEffect(() => {
+    let mouseDown = false;
+
+    let dragIcon = document.querySelector("#scorecard-drag-icon");
+    let scorecardContainer = document.querySelector(".map-scorecard-container");
+
+    let mouseX;
+    let mouseY;
+
+    let elOffsetX;
+    let elOffsetY;
+
+    document.addEventListener("mousemove", trackMouse);
+
+    dragIcon.addEventListener("mousedown", trackMouseInEl);
+
+    function trackMouseInEl(event) {
+      elOffsetX = event.offsetX;
+      elOffsetY = event.offsetY;
+    }
+
+    function trackMouse(event) {
+      mouseX = event.pageX;
+      mouseY = event.pageY;
+
+      if (mouseDown) {
+        scorecardContainer.style.transform = `translate(${mouseX -
+          scorecardContainer.offsetLeft -
+          elOffsetX -
+          164}px, ${mouseY - scorecardContainer.offsetTop - elOffsetY - 4}px)`;
+      }
+    }
+
+    dragIcon.addEventListener("mousedown", () => {
+      mouseDown = true;
+    });
+
+    dragIcon.addEventListener("mouseup", () => {
+      mouseDown = false;
+    });
+  });
+
   return (
-    <div className="map-scorecard-container">
+    <div id="map-scorecard-container" className="map-scorecard-container">
+      <DragIcon id="scorecard-drag-icon" />
       <input
         className="scorecard-checkbox"
         id="past"
