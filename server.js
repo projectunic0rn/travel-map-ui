bd6ea67e68c5c507a8867d0361311dfaa72fd09a
Matt Vance <mvance43776@gmail.com>
2019-12-07 21:12:09 -0800

    localhost

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
 
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 const app = express();
-app.use(favicon(__dirname + "/build/icon.PNG"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
@@ -14,4 +13,4 @@ app.get("/ping", function(req, res) {
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, host, () => console.log(`Server running on port ${port}`));
