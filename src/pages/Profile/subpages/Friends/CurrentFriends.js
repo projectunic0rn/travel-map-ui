6bb1021fce7223eb111507f81294dc6c9465d244
Matt Vance <mvance43776@gmail.com>
2020-04-15 00:11:24 -0700

    fixed profile friends loader centering, profile friends background height, profile travel tags save button, profile settings no social text

--- src/pages/Profile/subpages/Friends/CurrentFriends.js
+++ src/pages/Profile/subpages/Friends/CurrentFriends.js
@@ -4,7 +4,7 @@ import { Query } from "react-apollo";
 import { GET_ALL_USER_INFO } from "../../../../GraphQL";
 
 import FriendCard from "./FriendCard";
-import Loader from "../../../../components/common/Loader/Loader";
+import SimpleLoader from "../../../../components/common/SimpleLoader/SimpleLoader";
 
 export default function CurrentFriends({ searchText }) {
   const [filteredFriendsAvailable, handleFilteredFriendsAvailable] = useState(
@@ -35,11 +35,11 @@ export default function CurrentFriends({ searchText }) {
       onCompleted={() => handleLoaded(true)}
     >
       {({ loading, error, data, refetch }) => {
-        if (loading) return <Loader />;
+        if (loading) return <div className = 'centered-loader'><SimpleLoader /></div>;
         if (error) return `Error! ${error}`;
         handleFriends(data.users);
         handleFilteredFriendsAvailable(data.users);
-        if (!loaded) return <Loader />;
+        if (!loaded) return <SimpleLoader />;
         return filteredFriendsAvailable.map(friend => (
           <FriendCard key={friend.id} friend={friend} currentFriend={true} />
         ));
